9ee978c4563912f1f9f594d659b880eb8155c9c8
Emily Wise <wise.m.emily@gmail.com>
2017-12-18 16:56:59 -0700

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171218232856) do
+ActiveRecord::Schema.define(version: 20171218235512) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,8 +59,8 @@ ActiveRecord::Schema.define(version: 20171218232856) do
     t.string "image_content_type"
     t.integer "image_file_size"
     t.datetime "image_updated_at"
-    t.bigint "stores_id"
-    t.index ["stores_id"], name: "index_orders_on_stores_id"
+    t.bigint "store_id"
+    t.index ["store_id"], name: "index_orders_on_store_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
