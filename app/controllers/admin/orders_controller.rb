e067a8b10ad8f695d62a55f0373028c8e460bf2e
Emily Wise <wise.m.emily@gmail.com>
2017-12-18 17:16:20 -0700

    I can see all orders

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,6 +2,7 @@ class Admin::OrdersController < ApplicationController
 
   def index
     @store = current_user.stores.find_by(url: params[:store_name])
+    @orders = @store.orders
   end
 
   def show
