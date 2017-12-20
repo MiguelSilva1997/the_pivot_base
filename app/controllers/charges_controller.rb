c9d7c1538ebada60ac24c54c2d3b9f6f4abeccca
Miguel Silva <msilva0897@gmail.com>
2017-12-19 22:54:51 -0700

    creating the stripe functionality

---
+++ app/controllers/charges_controller.rb
@@ -0,0 +1,63 @@
+class ChargesController < ApplicationController
+  before_action :total_cost
+
+  def new
+
+  end
+
+  def create
+    create_customer
+    create_charge
+    checkout_method
+    redirect_to user_orders_path(current_user)
+    rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_to new_charge_path
+  end
+
+  private
+
+  def total_cost
+    @amount = params[:total]
+  end
+
+  def create_customer
+    @customer = StripeTool.create_customer(
+      :email => params[:stripeEmail],
+      :source  => params[:stripeToken]
+    )
+  end
+
+  def create_charge
+    StripeTool.create_charge(
+      :customer_id    => @customer.id,
+      :amount      => @amount,
+      :description => 'Rails Stripe customer'
+    )
+    current_user.update!(stripe_token: params[:stripeToken], stripe_customer_id: @customer.id)
+  end
+
+  def create_order
+    item_hash = @cart.cart_items
+    item_hash.each do |item, quantity|
+      order = Order.create(status: "paid", user_id: current_user.id, store_id: item.store.id)
+      order.order_items.create(quantity: quantity, original_item_price: item.price, item: item)
+    end
+    @cart.destroy
+    flash[:success] = "Order was successfully placed"
+  end
+
+  def pay_order
+    order = current_user.orders.find(params[:order])
+    order.update(status: 'paid')
+  end
+
+  def checkout_method
+    if params[:order]
+      pay_order
+    else
+      create_order
+    end
+  end
+
+end
