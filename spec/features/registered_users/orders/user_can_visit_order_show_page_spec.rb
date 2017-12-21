5f1b841d4f8a9a925fef1fbc2261d4a966418ff2
Emily Wise <wise.m.emily@gmail.com>
2017-12-20 17:41:22 -0700

    user show page

--- spec/features/registered_users/orders/user_can_visit_order_show_page_spec.rb
+++ spec/features/registered_users/orders/user_can_visit_order_show_page_spec.rb
@@ -1,18 +1,19 @@
 require 'rails_helper'
 
 describe "As a user" do
-  describe "visits /orders" do
-    it "can see all past orders" do
+  describe "visit  user_order_path" do
+    it "can see the details for the past order" do
       user = create(:user)
       item = create(:item, price: 5.00)
       items_with_quantity = [ {item => 2} ]
       order = create(:order_with_items, user: user, items_with_quantity: items_with_quantity)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-      visit '/orders'
+      visit user_orders_path(user)
       click_on order.id
+      
 
-      expect(current_path).to eq(order_path(order))
+      expect(current_path).to eq(user_order_path(user, order))
       expect(page).to have_content("Total: $10.00")
     end
   end
