a339ca73d4870f86844b397061968af3ea4387b7
Emily Wise <wise.m.emily@gmail.com>
2017-12-20 22:14:27 -0700

    user_spec

--- spec/models/store_user_spec.rb
+++ spec/models/store_user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe StoreUser, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'realtionships' do
+
+    it 'belongs to a user' do
+      storeUser = build(:store_user)
+      expect(storeUser).to respond_to(:user)
+    end
+
+    it 'belongs to a store' do
+      storeUser = build(:store_user)
+      expect(storeUser).to respond_to(:store)
+    end
+    it 'belongs to a role' do
+      storeUser = build(:store_user)
+      expect(storeUser).to respond_to(:role)
+    end
+  end
 end
