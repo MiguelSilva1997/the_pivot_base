2cb10f06be0113747a68efa0036ebd670bf37fde
Emily Wise <wise.m.emily@gmail.com>
2017-12-20 18:54:38 -0700

    user can add something to the cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,11 +20,6 @@ class ApplicationController < ActionController::Base
     def authorize!
       current_permission = PermissionService.new(current_user, params[:controller], params[:action])
       not_found unless current_permission.authorized?
-
-<<<<<<< HEAD
-=======
-
->>>>>>> user can add something to the cart
     end
 
     def not_found
