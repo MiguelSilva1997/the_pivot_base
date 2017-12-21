31aa04a5896596b2183ee0b6e1b79631186b6917
Aurora Ziobrowski <aziobrow@gmail.com>
2017-12-21 13:04:07 -0700

    cart tests

--- spec/features/registered_users/cart/registered_user_can_check_out_spec.rb
+++ spec/features/registered_users/cart/registered_user_can_check_out_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe 'As a user' do
       expect(page).to have_content("You now have 1 #{store.items.first.title}")
 		end
 
-    it "a user can change quantity in cart" do
+    it "a user can increase quantity in cart" do
       user = create(:user)
       store = create(:store_with_item)
 			stub_login(user)
@@ -23,11 +23,42 @@ RSpec.describe 'As a user' do
       click_on "Add to Cart"
 
       visit cart_path
+      click_on("Increase")
       save_and_open_page
-      second(".badge").click
-      # click_on(class: 'badge')
+
 
       expect(page).to have_content("2")
+    end
+
+    it "a user can decrease quantity in cart" do
+      user = create(:user)
+      store = create(:store_with_item)
+			stub_login(user)
+
+      visit store_path(store.url)
+      click_on "Add to Cart"
+      click_on "Add to Cart"
+
+      visit cart_path
+      click_on("Decrease")
+
+
+      expect(page).to have_content("1")
+    end
+
+    it "a user can remove item from cart" do
+      user = create(:user)
+      store = create(:store_with_item)
+			stub_login(user)
+
+      visit store_path(store.url)
+      click_on "Add to Cart"
+
+      visit cart_path
+      click_on("Remove")
+
+
+      expect(page).to have_content("Successfully removed #{store.items.first.title} from your cart.")
     end
 	end
 end
