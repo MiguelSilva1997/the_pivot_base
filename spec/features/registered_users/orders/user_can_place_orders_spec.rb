7c9511c61e280957ef83a38d42aa0edbbb5c480e
Emily Wise <wise.m.emily@gmail.com>
2017-12-20 20:41:32 -0700

    user can place an order

--- spec/features/registered_users/orders/user_can_place_orders_spec.rb
+++ spec/features/registered_users/orders/user_can_place_orders_spec.rb
@@ -1,16 +1,35 @@
 require 'rails_helper'
 
-RSpec.feature "User can place an order" do
-  it "and see the message order was successfully placed'" do
-    user = create(:user)
-    item = create(:item)
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+feature "User can place an order" do
+    let(:user) {create(:user)}
+    let(:item) {create(:item)}
 
-    visit store_item_path(item.store.url, item.url)
+      before(:each) do
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+        visit store_item_path(item.store.url, item.url)
+        click_on "Add to Cart"
+      end
 
-
-    click_on "Add to Cart"
+  it "User can add an item to the cart" do
 
     expect(page).to have_content("You now have 1 #{item.title}.")
+  end
+
+  it "Can checkout if logged in as a user without Stripe" do
+    visit cart_path
+    click_on "Checkout"
+
+    expect(page).to have_content("Order was successfully placed")
+    expect(current_path).to eq(user_orders_path(user))
+
+  end
+
+  it "Can checkout with Stripe" do
+    visit cart_path
+    click_on "Checkout Instantly with Stripe"
+
+    expect(current_path).to eq(new_charge_path)
+  
+    expect(page).to have_content("Amount ")
   end
 end
